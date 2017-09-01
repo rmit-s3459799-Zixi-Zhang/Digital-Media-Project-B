d57e9fc0509b07034b92b064e96546659e495a37
dengzexi <s3441278@student.rmit.edu.au>
2017-09-01 18:19:17 +1000

    improvement on control  add power bar

--- Assets/Script/Player_Control.cs
+++ Assets/Script/Player_Control.cs
@@ -8,8 +8,13 @@ public class Player_Control : MonoBehaviour
 	public Camera camera;
 	private Vector3 screenPos;
 	public float speed;
+	public float maxSpeed;
 	private Rigidbody rb;
 	private Vector3 shootDirection;
+	private bool isflying;
+	private Slider powerBar;
+
+	private float rate;
 
 	void Awake()
 	{
@@ -17,26 +22,51 @@ public class Player_Control : MonoBehaviour
 	}
 	void Start () {
 		
+		isflying = false;
+		powerBar= GameObject.Find("Power").GetComponent<Slider>();
 
 	}
-	void FixedUpdate() {
+	void Update() {
+
+
+
+
+
 		rb = GetComponent<Rigidbody>();
 		Vector3 mousePosition = Input.mousePosition;
+		Debug.DrawLine (rb.position, mousePosition, Color.red);
 
-		//screenPos = camera.ScreenToWorldPoint(Vector3(mousePosition.x, mousePosition.y, transform.position.z - camera.main.transform.position.z));
 		rb.transform.eulerAngles = new Vector3(0,0,Mathf.Atan2((mousePosition.y - transform.position.y), (mousePosition.x - transform.position.x))*Mathf.Rad2Deg - 90);
-
-
-		//Vector3 worldpos = camera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, mousePosition.z));
-	 	//rb.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg - 90);
-		//Vector3 turretLookDirection = new Vector3 (worldpos.x,rb.transform.position.y, worldpos.z);
-		//rb.transform.LookAt(turretLookDirection);
-		print (mousePosition);
-		if (Input.GetMouseButton (0)) {
-			
-			shootDirection = mousePosition;
-			shootDirection = shootDirection - rb.transform.position;
-			rb.velocity = new Vector2 (shootDirection.x * speed, shootDirection.y * speed);
+		if(speed <= 0)
+		{
+			rate = 1;
 		}
+		if (speed >= maxSpeed) {
+			rate = -1;
+		}
+		if (Input.GetMouseButton(0)&&isflying==false) {
+			speed = speed+(Time.deltaTime*rate); 
+			powerBar.value = speed;
+		}
+		if (Input.GetMouseButtonUp(0)&&isflying==false) {
+			shootDirection = mousePosition - rb.transform.position;
+			rb.velocity = new Vector2 (shootDirection.x * speed, shootDirection.y * speed);
+			isflying = true;
+			powerBar.value = 0;
+		}
+	
+	}
+	void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
+	{
+		GameObject myLine = new GameObject();
+		myLine.transform.position = start;
+		myLine.AddComponent<LineRenderer>();
+		LineRenderer lr = myLine.GetComponent<LineRenderer>();
+		lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+		lr.SetColors(color, color);
+		lr.SetWidth(0.1f, 0.1f);
+		lr.SetPosition(0, start);
+		lr.SetPosition(1, end);
+		GameObject.Destroy(myLine, duration);
 	}
 }
