d70b7dd5094564222b4660249ea1030dcbe21eeb
dengzexi <s3441278@student.rmit.edu.au>
2017-09-12 21:02:34 +1000

    add Atmosphere

--- Assets/Script/Atmosphere.cs
+++ Assets/Script/Atmosphere.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Atmosphere : MonoBehaviour {
-	
+	public GameObject Tartget;
+	public float Transparency;
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		SpriteRenderer.color = new Color (1f, 1f, 1f, 5f);
+		SpriteRenderer spRend = Tartget.transform.GetComponent<SpriteRenderer>();
+		// copy the SpriteRenderer's color property
+		Color col = spRend.color;
+		//  change col's alpha value (0 = invisible, 1 = fully opaque)
+		col.a = Transparency; // 0.5f = half transparent
+		// change the SpriteRenderer's color property to match the copy with the altered alpha value
+		spRend.color = col;
 	}
 }
